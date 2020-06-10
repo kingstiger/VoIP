1b6a900276b7daf603ba31437d330078ff39bd1e
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2020-06-11 00:15:49 +0200

    fixed

--- src/main/java/com/models/UserTO.java
+++ src/main/java/com/models/UserTO.java
@@ -3,8 +3,6 @@ package com.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-import java.util.List;
-
 @Data
 @Builder
 @ToString
@@ -13,21 +11,20 @@ import java.util.List;
 public class UserTO {
     private String userID;
 
-    private boolean active = false;
     private boolean favourite = false;
 
     private String username;
     private String email;
-    private List<UserShortDAO> favourites;
     @JsonProperty("ipaddress")
     private String IPAddress;
 
     public UserShortTO map() {
         return UserShortTO.builder()
-                    .userID(getUserID())
-                     .username(getUsername())
-                     .favourite(isFavourite())
-                     .IPAddress(getIPAddress())
+                .userID(getUserID())
+                .username(getUsername())
+                .favourite(isFavourite())
+                .IPAddress(getIPAddress())
+                .active(false)
                      .build();
     }
 }
